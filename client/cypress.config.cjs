da6afc77d197c51d3e2c8055c0d5b17301de069e
Vaishnav Verma <vermavaishnav70@gmail.com>
2026-03-20 13:03:12 +0530

    fix: remove cypress require to fix npx cache resolution error

--- client/cypress.config.cjs
+++ client/cypress.config.cjs
@@ -1,10 +1,8 @@
-const { defineConfig } = require("cypress");
-
-module.exports = defineConfig({
+module.exports = {
   e2e: {
     baseUrl: 'http://localhost:5173',
     setupNodeEvents() {
       // implement node event listeners here
     },
   },
-});
+};
